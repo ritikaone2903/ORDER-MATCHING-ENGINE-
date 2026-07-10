153ee8ed6c95905b3f1d35c613e0237321186b99
ritikaone2903 <ritikaone2903@gmail.com>
2026-07-10 14:15:20 +0530

    middleware

---
+++ ordercontroller.js
@@ -0,0 +1,25 @@
+function logger(req,res,next) {//this func will log every request 
+    console.log('${req.method} ${req.url}');
+    next();//this will pass the control to the next handler func or middleware
+}
+
+
+function authenticate(req,res,next) {
+    const token = req.headers.authorization;
+    if(!token) return res.status(401).json({ error: 'no token provided'});
+    //verfiying token 
+
+
+
+    next();
+}
+
+
+
+app.use(logger); // that this applies to  all the routes
+app.get('./orders',authenticate, (req,res) =>{//this will only run if authentication is done and control passed to next func 
+    res.json({ orders: []});
+});
+
+
+
